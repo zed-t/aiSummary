08e995098c228a795bc29612cf8536c2952a2156
m3lodicaman <kai.turanski@mail.mcgill.ca>
2023-03-13 15:41:43 -0400

    Made executable, updated functions
    Updated README, made .exe executable, improved line slicing, added dialog panes

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # Summarize PDFs Using GPT3
-#### Original by Zed Tarar
-#### Improved by Kai Turanski
+##### Original by Zed Tarar
+##### Improved by Kai Turanski
 
 ### Python code to help you skim through PDFs
-Obviously there are limitations here, for example, paragraphs that bleed from one page to another. This version has eliminated previous bugs by dividing the PDF into chunks readable by GPT3 and by making the script work anywhere on your computer. 
+This program allows you to select a PDF, divides it into chunks readable by GPT3, and creates a summary of it next to the original PDF. 
 
-### Install
-1. Use the python file and make sure you have the right libraries installed. sys, math, and os should be local to your computer. Here are links for the [openai](https://github.com/openai/openai-python) and [PyPDF2](https://pypdf2.readthedocs.io/en/3.0.0/user/installation.html) libraries.
+### Current limitations
+- GPT3 may have a hard time interpreting paragraphs that bleed from one chunk to another
+- Text output can sometimes contain badly formatted text, which can usually be deleted easily with no harm to comprehension
+
+### Editing 
+1. To edit the python file, make sure you have the right libraries installed. ```sys```, ```math```, and ```os``` should be local to your computer. Here are links for the [```openai```](https://github.com/openai/openai-python), [```PyPDF2```](https://pypdf2.readthedocs.io/en/3.0.0/user/installation.html), and [```tkinter```](https://www.tutorialspoint.com/how-to-install-tkinter-in-python) libraries.
 2. You'll need to get your API key from OpenAI (ask chat GPT how to do that), then copy it into the script where indicated 
 3. The output is both printed on the command line and saved to ```summary of <your document name>.txt```
 
-### Ease of access
-This guide will show you how to set up a custom command to use this program anywhere on your computer via command line
+### Usage
 
-1. Open the command line
-2. Enter ```cd ~``` and then ```ls -a```
-3. Open you bash config file (.zshrc on Mac and .bashrc on Linux) with ```open -e <file name>```
-4. Put your absolute path to the python file as shown and then copy this line to the end of your bash config file ```alias summarize="python3 <absolute path to file>/pdfExtract3.py $1"``` 
-5. You're done! Now go to the directory of the file you want to summarize and run ```summarize "name of the file you want to summarize"```
+1. Navigate to your ```./dist/<your operating system>_version_pdf_extract/<your operating system>_version_pdf_extract.<extension>``` file
+2. Double click to run the file and you're done!
 
 ### Contribute
 There are probably more elegant ways to create summaries from long PDFs, so if you have them, please share! 
--- pdfExtract3.py
+++ pdfExtract3.py
@@ -2,32 +2,42 @@ import os.path
 import sys
 import math
 import openai
-import PyCryptodome
+#import PyCryptodome
 from PyPDF2 import PdfReader
+import tkinter as tk
+from tkinter import filedialog 
+from tkinter import messagebox
 
-def uniquify(path):
-    filename, extension = os.path.splitext(path)
+def uniquify(name, goalPath):
+    filename, extension = os.path.splitext(name)
+    tempName = filename + extension
+    path = os.path.join(goalPath, tempName)
     counter = 1
 
     while os.path.exists(path):
-        path = filename + " (" + str(counter) + ")" + extension
+        tempName = filename + " (" + str(counter) + ")" + extension
+        path = os.path.join(goalPath, tempName)
         counter += 1
 
     return path
+
+def getTargetPDF():
+    root = tk.Tk()
+    root.withdraw()
+
+    messagebox.showinfo("Prompt","Please select a file to summarize")
+    file_path = filedialog.askopenfilename()
+    return file_path
 
 
 # Divide option and arguement inputs into two arrays
 opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
 args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-# Get name of PDF from args
-if len(args) > 0:
-    pdfName = str(args[0])
-else:
-    sys.exit("No filename passed for summary");
+pdfName = getTargetPDF()
 
 # Create an instance of the GPT-3 model
-openai.api_key = "!!!put your GPT-3 key here!!!"
+openai.api_key = "!!! Your GPT Key Here !!!"
 
 # open the PDF file
 pdfFile = open(pdfName, 'rb')
@@ -36,53 +46,86 @@ pdfFile = open(pdfName, 'rb')
 pdfReader = PdfReader(pdfFile)
 meta = pdfReader.metadata;
 
-print("Printing the document info:")
+# set max length of input to GPT3
+acceptableLengthGPT = 3297*2
+
+# set path of output summary file
+summaryPath = uniquify("summary of "+os.path.basename(pdfName).split(".")[0]+".txt", os.path.dirname(pdfName))
+
+# set document metadata
+documentData = "Document info:"
 if(meta is not None):
-    print("\nAuthor: " + str(meta.author)
-        +"\nTitle: " + str(meta.title))
-print("- - - - - - - - - - - - - - - - - - - -")
-print("Number of Pages: " + str(len(pdfReader.pages)))
+    documentData = documentData +"\nAuthor: " + str(meta.author) +"\nTitle: " + str(meta.title)
+documentData = documentData+"\n- - - - - - - - - - - - - - - - - - - -"+"\nNumber of Pages: " + str(len(pdfReader.pages))
+
+# display and save document metadata
+print(documentData)
+with open(summaryPath, 'a') as f:
+        # Write document metadata to the file
+        f.writelines(documentData)
 
 numOfPages = len(pdfReader.pages)
 rawText = ""
 pageEndIndices = []
 
+# parse all text into single string
 for i in range(0, numOfPages):
     nextText = pdfReader.pages[i].extract_text()
     rawText = rawText + nextText
     pageEndIndices.append(len(rawText))
 
-acceptableLengthGPT = 3297
-summaryPath = uniquify("summary of "+os.path.basename(pdfName).split(".")[0]+".txt")
-
+end = 0
+# send text to GPT3 in batches
 for i in range(0, math.floor(len(rawText)/acceptableLengthGPT)):
-    # Divide the text into lengths accepted by GPT3 (<3297 chars)
-    if((i+1)*acceptableLengthGPT<len(rawText)):
-        targetText = rawText[i*acceptableLengthGPT:(i+1)*acceptableLengthGPT]
-    else:
-        targetText = rawText[i*acceptableLengthGPT:len(rawText)]
+    start = end
 
+    # find end of batch
+    if(end+acceptableLengthGPT < len(rawText)):
+        end = end+acceptableLengthGPT
+    else:
+        end = len(rawText)
+
+    # find index of last end of sentence in batch
+    for j in range(0, acceptableLengthGPT):
+        if(rawText[end] == "."):
+            end += 1
+            break
+        else:
+            end -= 1
+
+    #print("Start and end indexed: "+rawText[start]+", "+rawText[end-1])
+
+    # divide the text into lengths accepted by GPT3 (<3297 chars)
+    targetText = rawText[start:end]
+
+    # get which pages are currently being read from
     pages = []
     for j in range(0, numOfPages):
-        if(i*acceptableLengthGPT<=pageEndIndices[j]<(i+1)*acceptableLengthGPT):
+        if(((i == 0 and j == 0) 
+            or (j > 0 and start <= pageEndIndices[j-1] + 1 < end)) 
+            or start <= pageEndIndices[j] < end):
             pages.append(j+1)
 
     response = openai.Completion.create(
     engine="text-davinci-003",
-    prompt="Write a summary for this in succint style: " + targetText,
+    prompt="Summarize this text in the style of the New Yorker: " + targetText,
     max_tokens=800,
     n=1,
-    temperature=0.5,
+    temperature=0.3,
+    presence_penalty=0.1,
 )
 
     # Print the generated summary
     print("\n\nPage(s) "+str(pages)+":\n"+response["choices"][0]["text"])
 
     # Open a text file for writing
-    with open(summaryPath, 'a') as f:
+    with open(summaryPath, 'a', encoding="utf-8") as f:
         # Write the list of bullet point summaries to the file
         f.writelines("\n\nPage(s) "+str(pages)+":\n"+response["choices"][0]["text"])
 
 # close the PDF file object
 pdfFile.close()
+
+# open summary file
+os.startfile(summaryPath)
 
